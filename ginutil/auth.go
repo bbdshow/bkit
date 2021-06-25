2aa3f7896003cdec34bad9af6dda916df21dacd2
zhongqing2016 <zhongqing2016@qq.com>
2021-06-25 17:15:05 +0800

    fix auth

--- ginutil/auth.go
+++ ginutil/auth.go
@@ -6,22 +6,14 @@ import (
 	"time"
 )
 
-type JWTData struct {
-	Uid      int64       `json:"uid"`
-	Role     string      `json:"role"`
-	NickName string      `json:"nickName"`
-	Phone    string      `json:"phone"`
-	Data     interface{} `json:"data"`
-}
+var JWTDataKey = "jwt_claims_data"
 
-var JWTDataKey = "jwt_data_key"
-
-func GenJWTToken(data JWTData, ttl time.Duration, signingKey ...string) (token string, err error) {
+func GenJWTToken(data interface{}, ttl time.Duration, signingKey ...string) (token string, err error) {
 	return jwt.GenerateJWTToken(jwt.NewCustomClaims(data, ttl, signingKey...))
 }
 
 func SetJWTDataToContext(c *gin.Context, token string, signingKey ...string) error {
-	data := JWTData{}
+	var data interface{}
 	if err := jwt.GetCustomData(token, &data, signingKey...); err != nil {
 		return err
 	}
@@ -29,22 +21,10 @@ func SetJWTDataToContext(c *gin.Context, token string, signingKey ...string) err
 	return nil
 }
 
-func GetJWTDataFromContext(c *gin.Context) (data JWTData, exists bool) {
+func GetJWTDataFromContext(c *gin.Context) (data interface{}, exists bool) {
 	val, ok := c.Get(JWTDataKey)
 	if !ok {
 		return data, ok
 	}
-	data, ok = val.(JWTData)
-	if !ok {
-		return data, ok
-	}
-	return data, true
-}
-
-func JWTDataUidIsEqual(c *gin.Context, uid int64) bool {
-	data, exists := GetJWTDataFromContext(c)
-	if !exists {
-		return exists
-	}
-	return data.Uid == uid
+	return val, true
 }
